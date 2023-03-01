1ce0a04617a36b453f7435c6ce1c815f4f2ea096
bravin-atonya <bravinatonya.brani@gmail.com>
2023-03-01 02:55:35 -0800

    betty compliance

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,14 +3,23 @@
 #include <stddef.h>
 #include <stdio.h>
 
-binary_tree_t* binary_tree_node(binary_tree_t *parent, int value)
+/*
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: the parent of the tree node
+ * @value: the value of the new node
+ * Return: the new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t* parent_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *parent_node = malloc(sizeof(binary_tree_t));
+
 	if (parent_node == NULL)
-		return NULL;
+	{
+		return (NULL);
+	}
 	parent_node->parent = parent;
 	parent_node->left = NULL;
 	parent_node->right = NULL;
 	parent_node->n = value;
-	return parent_node;
+	return (parent_node);
 }
